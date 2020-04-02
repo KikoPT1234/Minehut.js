17ec4df59e3d27a6bc25d7fdf2c559ad333b308e
KikoPT1234 <carricokiko2006@gmail.com>
2020-04-02 22:43:51 +0200

    v2.1

--- generated/classes/Session.js
+++ generated/classes/Session.js
@@ -6,70 +6,76 @@ const User_1 = require("./User");
 const APIError_1 = require("./APIError");
 const fetch = require("node-fetch");
 class Session {
-    constructor(credentials, onceLogged) {
+    constructor() {
+        this.loggedIn = false;
+    }
+    async login(credentials) {
         if (!(credentials && credentials.email && credentials.password))
             throw new Error("Invalid credentials.");
-        fetch("https://api.minehut.com/users/login", {
+        let session = await fetch("https://api.minehut.com/users/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(credentials)
-        }).then(async (session) => {
-            let e;
-            session = await session.json();
-            if (session.error)
-                return onceLogged(new APIError_1.APIError(session.error.replace("Error: ", "")));
-            this.id = session.sessionId;
-            this.token = session.token;
-            const response1 = this.fetch(`https://api.minehut.com/user/${session._id}`);
-            const response2 = this.fetch(`https://api.minehut.com/user/${session._id}/credit/transactions`);
-            const response3 = this.fetch(`https://api.minehut.com/user/${session._id}/payments`);
-            let [{ user }, { transactions }, { payments }] = await Promise.all((await Promise.all([response1, response2, response3])).map(r => r.json()));
-            let transactionCollection = new collection_1.default();
-            transactions.forEach((t) => {
-                const newT = {};
-                const id = t._id;
-                Object.keys(t).forEach(k => {
-                    let key = k;
-                    if (key === "_id")
-                        key = "id";
-                    if (key === "__v")
-                        key = "v";
-                    key = key.replace(/_(.)/g, e => e[1].toUpperCase());
-                    newT[key] = t[k];
-                });
-                transactionCollection.set(id, newT);
-            });
-            const paymentCollection = new collection_1.default();
-            payments.forEach((p) => {
-                const newT = {};
-                const id = p._id;
-                Object.keys(p).forEach(k => {
-                    let key = k;
-                    if (key === "_id")
-                        key = "id";
-                    if (key === "__v")
-                        key = "v";
-                    key = key.replace(/_(.)/g, e => e[1].toUpperCase());
-                    newT[key] = p[k];
-                });
-                paymentCollection.set(id, newT);
-            });
-            this.user = new User_1.User({
-                ...user,
-                transactions: transactionCollection,
-                payments: paymentCollection
-            }, this);
-            this.user.servers = new collection_1.default();
-            let servers = await this.fetch(`https://api.minehut.com/servers/${this.user.id}/all_data`);
-            servers = await servers.json();
-            servers = servers.map((server) => new Server_1.SessionServer(server, this.user, this));
-            servers.forEach((server) => this.user.servers.set(server.id, server));
-            onceLogged();
         });
+        session = await session.json();
+        if (session.error) {
+            this.loggedIn = false;
+            throw new APIError_1.APIError(session.error.replace("Error: ", ""));
+        }
+        this.id = session.sessionId;
+        this.token = session.token;
+        this.loggedIn = true;
+        const response1 = this.fetch(`https://api.minehut.com/user/${session._id}`);
+        const response2 = this.fetch(`https://api.minehut.com/user/${session._id}/credit/transactions`);
+        const response3 = this.fetch(`https://api.minehut.com/user/${session._id}/payments`);
+        let [{ user }, { transactions }, { payments }] = await Promise.all((await Promise.all([response1, response2, response3])).map(r => r.json()));
+        let transactionCollection = new collection_1.default();
+        transactions.forEach((t) => {
+            const newT = {};
+            const id = t._id;
+            Object.keys(t).forEach(k => {
+                let key = k;
+                if (key === "_id")
+                    key = "id";
+                if (key === "__v")
+                    key = "v";
+                key = key.replace(/_(.)/g, e => e[1].toUpperCase());
+                newT[key] = t[k];
+            });
+            transactionCollection.set(id, newT);
+        });
+        const paymentCollection = new collection_1.default();
+        payments.forEach((p) => {
+            const newT = {};
+            const id = p._id;
+            Object.keys(p).forEach(k => {
+                let key = k;
+                if (key === "_id")
+                    key = "id";
+                if (key === "__v")
+                    key = "v";
+                key = key.replace(/_(.)/g, e => e[1].toUpperCase());
+                newT[key] = p[k];
+            });
+            paymentCollection.set(id, newT);
+        });
+        this.user = new User_1.User({
+            ...user,
+            transactions: transactionCollection,
+            payments: paymentCollection
+        }, this);
+        this.user.servers = new collection_1.default();
+        let servers = await this.fetch(`https://api.minehut.com/servers/${this.user.id}/all_data`);
+        servers = await servers.json();
+        servers = servers.map((server) => new Server_1.SessionServer(server, this.user, this));
+        servers.forEach((server) => this.user.servers.set(server.id, server));
+        return this;
     }
     async fetch(url, method, body) {
+        if (!this.loggedIn)
+            throw new Error("Not logged in.");
         if (!(this.id && this.token))
             throw new Error("Credentials not found.");
         const settings = {
--- src/classes/Session.ts
+++ src/classes/Session.ts
@@ -8,64 +8,69 @@ export class Session {
     user: User
     id: string
     token: string
-    constructor(credentials: LoginCredentials, onceLogged: Function) {
+    loggedIn = false
+    async login(credentials: LoginCredentials) {
         if (!(credentials && credentials.email && credentials.password)) throw new Error("Invalid credentials.")
-        fetch("https://api.minehut.com/users/login", {
+        let session: {[key: string]: any} = await fetch("https://api.minehut.com/users/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(credentials)
-        }).then(async (session: {[key: string]: any}) => {
-            let e: APIError | Error
-            session = await session.json()
-            if (session.error) return onceLogged(new APIError(session.error.replace("Error: ", "")))
-            this.id = session.sessionId
-            this.token = session.token
-            const response1 = this.fetch(`https://api.minehut.com/user/${session._id}`)
-            const response2 = this.fetch(`https://api.minehut.com/user/${session._id}/credit/transactions`)
-            const response3 = this.fetch(`https://api.minehut.com/user/${session._id}/payments`)
-            let [{user}, {transactions}, {payments}] = await Promise.all((await Promise.all([response1, response2, response3])).map(r => r.json()))
-            let transactionCollection = new Collection()
-            transactions.forEach((t: {[key: string]: any}) => {
-                const newT: {[key: string]: any} = {}
-                const id = t._id
-                Object.keys(t).forEach(k => {
-                    let key = k
-                    if (key === "_id") key = "id"
-                    if (key === "__v") key = "v"
-                    key = key.replace(/_(.)/g, e => e[1].toUpperCase())
-                    newT[key] = t[k]
-                })
-                transactionCollection.set(id, newT)
-            })
-            const paymentCollection = new Collection()
-            payments.forEach((p: {[key: string]: any}) => {
-                const newT: {[key: string]: any} = {}
-                const id = p._id
-                Object.keys(p).forEach(k => {
-                    let key = k
-                    if (key === "_id") key = "id"
-                    if (key === "__v") key = "v"
-                    key = key.replace(/_(.)/g, e => e[1].toUpperCase())
-                    newT[key] = p[k]
-                })
-                paymentCollection.set(id, newT)
-            })
-            this.user = new User({
-                ...user,
-                transactions: transactionCollection,
-                payments: paymentCollection
-            }, this)
-            this.user.servers = new Collection()
-            let servers = await this.fetch(`https://api.minehut.com/servers/${this.user.id}/all_data`)
-            servers = await servers.json()
-            servers = servers.map((server: Server) => new SessionServer(server, this.user, this))
-            servers.forEach((server: SessionServer) => this.user.servers.set(server.id, server))
-            onceLogged()
         })
+        session = await session.json()
+        if (session.error) {
+            this.loggedIn = false
+            throw new APIError(session.error.replace("Error: ", ""))
+        }
+        this.id = session.sessionId
+        this.token = session.token
+        this.loggedIn = true
+        const response1 = this.fetch(`https://api.minehut.com/user/${session._id}`)
+        const response2 = this.fetch(`https://api.minehut.com/user/${session._id}/credit/transactions`)
+        const response3 = this.fetch(`https://api.minehut.com/user/${session._id}/payments`)
+        let [{user}, {transactions}, {payments}] = await Promise.all((await Promise.all([response1, response2, response3])).map(r => r.json()))
+        let transactionCollection = new Collection()
+        transactions.forEach((t: {[key: string]: any}) => {
+            const newT: {[key: string]: any} = {}
+            const id = t._id
+            Object.keys(t).forEach(k => {
+                let key = k
+                if (key === "_id") key = "id"
+                if (key === "__v") key = "v"
+                key = key.replace(/_(.)/g, e => e[1].toUpperCase())
+                newT[key] = t[k]
+            })
+            transactionCollection.set(id, newT)
+        })
+        const paymentCollection = new Collection()
+        payments.forEach((p: {[key: string]: any}) => {
+            const newT: {[key: string]: any} = {}
+            const id = p._id
+            Object.keys(p).forEach(k => {
+                let key = k
+                if (key === "_id") key = "id"
+                if (key === "__v") key = "v"
+                key = key.replace(/_(.)/g, e => e[1].toUpperCase())
+                newT[key] = p[k]
+            })
+            paymentCollection.set(id, newT)
+        })
+        this.user = new User({
+            ...user,
+            transactions: transactionCollection,
+            payments: paymentCollection
+        }, this)
+        this.user.servers = new Collection()
+        let servers = await this.fetch(`https://api.minehut.com/servers/${this.user.id}/all_data`)
+        servers = await servers.json()
+        servers = servers.map((server: Server) => new SessionServer(server, this.user, this))
+        servers.forEach((server: SessionServer) => this.user.servers.set(server.id, server))
+        return this
     }
-    async fetch(url: string, method?: string, body?: Object) {
+
+    async fetch(url: string, method?: string, body?: {[key: string]: any}) {
+        if (!this.loggedIn) throw new Error("Not logged in.")
         if (!(this.id && this.token)) throw new Error("Credentials not found.")
         const settings: {[key: string]: any} = {
             method: method || "GET",
