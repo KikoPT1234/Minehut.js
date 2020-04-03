7c0452e8b99c9827cdbabd4221d714676e5014a4
KikoPT1234 <carricokiko2006@gmail.com>
2020-04-03 13:38:40 +0200

    testing

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const Minehut = require("./generated/index")
 const session = new Minehut.Session()
+console.log(process.env.EMAIL, process.env.PASSWORD)
 session.login({
     email: process.env.EMAIL,
     password: process.env.PASSWORD
